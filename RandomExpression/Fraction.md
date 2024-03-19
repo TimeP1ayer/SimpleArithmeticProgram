68e3826b87fe702a826e6a8f0a2bee1b7d231707
Aderversa <2457699535@aliyun.com>
2024-03-19 20:10:14 +0800

    编写了一个Fraction类，为其添加了将分数正确转换成字符串的方法ToString()

---
+++ RandomExpression/Fraction.md
@@ -0,0 +1,18 @@
+# Fraction类
+
+提供一个类，用于实例化分数对象
+
+下面是其属性和接口定义
+
+属性：
+1. numerator表示分子
+2. denominator表示分母
+
+方法：
+1. `toSting()` 将返回分数的字符串形式(肯定返回真分数形式)。如果判断出该分数的分母为1，则说明这是个整数，就会返回整数字符串
+2. `getFraction()` 将返回一个分数对象，这个分数对象数据上跟自身是一样的。
+3. `Add()`返回一个Fraction相加后的Fraction
+4. `Sub()`返回一个Fraction相减后的Fraction
+5. `Mul()`返回一个Fraction相乘后的Fraction
+6. `Div()`返回一个Fraction相除后的Fraction
+
--- RandomExpression/src/peer/randomexp/Expression.java
+++ RandomExpression/src/peer/randomexp/Expression.java
@@ -1,47 +1,7 @@
 package peer.randomexp;
 
 public class Expression {
-    /**
-     * 整个表达式在每一次修改时变量都要更新
-     * result 表达式的结果
-     * peer.randomexp.Expression 表达式的字符串
-     * maxLeaf 表达树中的最大叶子值
-     * opType 子树操作符类型
+    /*
+    表达式类的基类，主要提供多态性，一般不使用这个对象
      */
-    private String result;
-    private String Expression;
-    private String maxLeaf;
-    private String opType;
-
-    public String getMaxLeaf() {
-        return maxLeaf;
-    }
-
-    public void setMaxLeaf(String maxLeaf) {
-        this.maxLeaf = maxLeaf;
-    }
-
-    public String getOpType() {
-        return opType;
-    }
-
-    public void setOpType(String opType) {
-        this.opType = opType;
-    }
-
-    public String getResult() {
-        return result;
-    }
-
-    public void setResult(String result) {
-        this.result = result;
-    }
-
-    public String getExpression() {
-        return Expression;
-    }
-
-    public void setExpression(String expression) {
-        Expression = expression;
-    }
 }
