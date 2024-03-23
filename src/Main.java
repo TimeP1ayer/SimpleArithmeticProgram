b76f0cd80ff6daf894b46cded0c0fe2497bd62f5
Aderversa <2457699535@aliyun.com>
2024-03-23 12:16:13 +0800

    修改了SubExpression的一些Bug

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,10 @@ public class Main {
         //测试用例
         String [] ak = {"-r","10","-e",Exercises,"-a",answer};
         String [] ak1 = {"-e",Exercises,"-a",answer};
+        String[] args0 = {"-n", "100", "-r", "20"};
 
         CommandUtil commandUtil = new CommandUtil();
-        commandUtil.getParameter(args);
+        commandUtil.getParameter(args0);
 
         //写入题目与答案
         if (commandUtil.getR()>0){
