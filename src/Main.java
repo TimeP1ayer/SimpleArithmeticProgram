98256927334b28153b0d502f79d585849456dee4
Aderversa <2457699535@aliyun.com>
2024-03-23 12:20:08 +0800

    改了一下main用于测试

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,9 @@ public class Main {
         //测试用例
         String [] ak = {"-r","10","-e",Exercises,"-a",answer};
         String [] ak1 = {"-e",Exercises,"-a",answer};
-        String[] args0 = {"-n", "100", "-r", "20"};
 
         CommandUtil commandUtil = new CommandUtil();
-        commandUtil.getParameter(args0);
+        commandUtil.getParameter(args);
 
         //写入题目与答案
         if (commandUtil.getR()>0){
